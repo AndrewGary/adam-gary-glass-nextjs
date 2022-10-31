0bd7da86b41df4c9f2f1a4409471f517b2035e58
Brenda Gary <brendagary@brendas-mbp.attlocal.net>
2022-10-31 15:51:10 -0500

    switching computers

---
+++ dummuData.ts
@@ -0,0 +1,38 @@
+export const dummyProducts = [
+    {
+        name: 'Fume Recycler Rig',
+        price: '100',
+        defaultImage: '/test.png',
+        images: ['test.png', 'test.png', 'test.png', 'test.png', 'test.png',],
+        description: 'This is a 10mm recycler accented with purple slyme. Great Function'
+    },
+    {
+        name: 'Fume Recycler Rig',
+        price: '100',
+        defaultImage: '/test.png',
+        images: ['test.png', 'test.png', 'test.png', 'test.png', 'test.png',],
+        description: 'This is a 10mm recycler accented with purple slyme. Great Function'
+    },
+    {
+        name: 'Fume Recycler Rig',
+        price: '100',
+        defaultImage: '/test.png',
+        images: ['test.png', 'test.png', 'test.png', 'test.png', 'test.png',],
+        description: 'This is a 10mm recycler accented with purple slyme. Great Function'
+    },
+    {
+        name: 'Fume Recycler Rig',
+        price: '100',
+        defaultImage: '/test.png',
+        images: ['test.png', 'test.png', 'test.png', 'test.png', 'test.png',],
+        description: 'This is a 10mm recycler accented with purple slyme. Great Function'
+    },
+    {
+        name: 'Fume Recycler Rig',
+        price: '100',
+        defaultImage: '/test.png',
+        images: ['test.png', 'test.png', 'test.png', 'test.png', 'test.png',],
+        description: 'This is a 10mm recycler accented with purple slyme. Great Function'
+    },
+    
+]
--- pages/Products/index.tsx
+++ pages/Products/index.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
-
+import { dummyProducts } from '../../dummuData'
+import Link from 'next/link';
 type Props = {}
 
 const index = (props: Props) => {
+
+  console.log(dummyProducts)
   return (
-    <div>index</div>
+    <div className='flex flex-col w-full min-h-screen items-center'>
+      {dummyProducts.map((product, i) => (
+        <div key={i} className='w-[90%] flex flex-col items-center relative'>
+          
+          {/* Image overlay div */}
+          <div className='space-y-3 absolute w-[70%] h-full z-10 flex flex-col items-center bg-slate-500 bg-opacity-75 opacity-0 hover:opacity-100 justify-center'>
+            <span className='text-red-500 font-extrabold'>{product.name}</span>
+            <span className='text-red-500'>${product.price}</span>
+
+            <Link className='w-3/4 text-center' href={'/'}>
+            <button className='w-full border border-red-500 rounded-lg'>View Product</button>
+            </Link>
+          </div>
+          
+          <img className='w-[70%] h-[75%]' src={product.defaultImage} alt='' />
+        </div>
+      ))}
+    </div>
   )
 }
 
