8f79221fab806bd3116f6fc628cdcc61ffa930d1
Andrew Gary <Andrew.gary91@gmail.com>
2022-11-01 01:11:45 -0500

    made some chages to the product info page

--- components/Header.tsx
+++ components/Header.tsx
@@ -31,7 +31,7 @@ const Header = (props: Props) => {
     <div className="w-full">
 
         <div className={`${searchActive ? '' : 'hidden'} w-full flex justify-between sticky top-0 bg-white`}>
-        <div className={`h-full border ml-2 mt-2`} onClick={() => {setHamburgerMenuActive(!hamburgerMenuActive)}}>
+        <div className={`h-full  ml-2 mt-2`} onClick={() => {setHamburgerMenuActive(!hamburgerMenuActive)}}>
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -48,7 +48,7 @@ const Header = (props: Props) => {
                 </svg>
             </div>
 
-            <div className="h-full border mt-2 flex space-x-2">
+            <div className="h-full  mt-2 flex space-x-2">
             
                 <input
                     type='text'
@@ -63,7 +63,7 @@ const Header = (props: Props) => {
         </Link>
             </div>
 
-            <div className="h-full border mr-2 mt-2 flex space-x-3">
+            <div className="h-full  mr-2 mt-2 flex space-x-3">
             <Link href='/Cart'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
         <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
         </svg>
@@ -104,7 +104,7 @@ const Header = (props: Props) => {
 
 
         <div className={`${hamburgerMenuActive || searchActive ? 'hidden' : ''} w-full flex justify-between sticky top-0 bg-white`}>
-            <div className={`h-full border ml-2 mt-2`} onClick={() => {setHamburgerMenuActive(!hamburgerMenuActive)}}>
+            <div className={`h-full  ml-2 mt-2`} onClick={() => {setHamburgerMenuActive(!hamburgerMenuActive)}}>
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -121,10 +121,12 @@ const Header = (props: Props) => {
                 </svg>
             </div>
 
-            <div className="h-full border mt-2">Adam Gary Glass
+            <Link href={'/'}>
+            <div className="h-full  mt-2">Adam Gary Glass
             </div>
+            </Link>
 
-            <div className="h-full border mr-2 mt-2 flex space-x-3">
+            <div className="h-full  mr-2 mt-2 flex space-x-3">
             <Link href='/Cart'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
         <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
         </svg>
