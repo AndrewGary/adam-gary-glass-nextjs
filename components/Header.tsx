8bdde60b984eae0f9e484e777e2b8e46b0ed41bb
Andrew Gary <Andrew.gary91@gmail.com>
2022-11-11 15:00:44 -0600

    added a way to sign in using google and Oauth

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 const options = [
 	{
@@ -17,7 +18,7 @@ const options = [
 	},
 	{
 		text: "Return Policy",
-		href: '/ReturnPolicy'
+		href: "/ReturnPolicy",
 	},
 	{
 		text: "Contact",
@@ -28,6 +29,8 @@ const options = [
 type Props = {};
 
 const Header = (props: Props) => {
+
+	const { data: session } = useSession();
 
 	const cartState = useSelector((state) => state.cart);
 	const { cart } = cartState;
@@ -157,17 +160,28 @@ const Header = (props: Props) => {
 					Close
 				</div>
 				<div className="w-full flex flex-col border-2 border-black">
-					{options.map((option, i) => (
-						<Link
-							key={i}
-							onClick={() => {
-								setHamburgerMenuActive(false);
-							}}
-							href={option.href}
-						>
-							{option.text}
-						</Link>
-					))}
+					{options.map((option, i) => {
+						if (option.text === "Login") {
+							// return <span onClick={() => {signIn()}}>hello</span>;
+							if(session){
+								return <span onClick={() => {signOut()}}>Sign Out</span>
+							}else{
+								return <span onClick={() => {signIn()}}>Sign In</span>;
+							}
+						} else {
+							return (
+								<Link
+									key={i}
+									onClick={() => {
+										setHamburgerMenuActive(false);
+									}}
+									href={option.href}
+								>
+									{option.text}
+								</Link>
+							);
+						}
+					})}
 				</div>
 			</div>
 
@@ -205,23 +219,25 @@ const Header = (props: Props) => {
 				<div className="h-full  mr-2 mt-2 flex space-x-3">
 					{/* <div className="absolute w-full"></div> */}
 					<div className="relative">
-						<div className="absolute w-3 h-3 rounded-full flex justify-center items-center left-3 text-[#ff3434] bottom-4 font-bold p-2">{cart.length}</div>
-					<Link href="/Cart">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							strokeWidth={1.5}
-							stroke="currentColor"
-							className="w-6 h-6"
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-							/>
-						</svg>
-					</Link>
+						<div className="absolute w-3 h-3 rounded-full flex justify-center items-center left-3 text-[#ff3434] bottom-4 font-bold p-2">
+							{cart.length}
+						</div>
+						<Link href="/Cart">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								strokeWidth={1.5}
+								stroke="currentColor"
+								className="w-6 h-6"
+							>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+								/>
+							</svg>
+						</Link>
 					</div>
 					<svg
 						onClick={() => {
