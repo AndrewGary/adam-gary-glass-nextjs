96b66499ebe8eb94c86d4cf5cf7951d1ef441765
Andrew Gary <Andrew.gary91@gmail.com>
2022-11-13 15:23:13 -0600

    The specific product details page was still using the dummy data so I fixed that

--- pages/Products/[id].tsx
+++ pages/Products/[id].tsx
@@ -1,71 +1,62 @@
-import React, { useEffect, useState } from 'react'
-import { useRouter } from 'next/router';
-import { addItem } from '../../store/cartSlice';
-import { useDispatch, useSelector } from 'react-redux';
-import { connectToDatabase } from '../../mongoConnection';
-import { ObjectId } from 'mongodb';
-import { GetStaticPaths, GetStaticProps } from 'next';
+import React from "react";
+import { addItem } from "../../store/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { connectToDatabase } from "../../mongoConnection";
+import { ObjectId } from "mongodb";
+import { GetStaticPaths, GetStaticProps } from "next";
 
-import { dummyProducts } from '../../dummuData';
-// const dummyProduct: {name: string, price: number, description: string, defaultImage: string, images: Array<string>} = {...dummyProducts[0]}
-const dummyProduct = {...dummyProducts[Math.floor(Math.random() * 5)]}
-
-type Props = {post: any}
+type Props = { post: any };
 
 const ProductDetails = (props: Props) => {
-  console.log('props: ', props);
-  const cart = useSelector((state: any) => state.cart.cart);
-  const dispatch = useDispatch();
+	console.log("props: ", props);
+	const dispatch = useDispatch();
 
-  const router = useRouter();
-  const { id } = router.query;
+	const handleAddToCart = () => {
+		dispatch(addItem({ ...props.post }));
+	};
 
+	return (
+		<div className="w-full flex flex-col min-h-screen items-center mt-4">
+			<div className="w-[90%] flex flex-col items-center">
+				<span>{props.post.name}</span>
 
-  const handleAddToCart = () => {
-    dispatch(addItem({...props.post}));
-  }
+				<div className="w-full overflow-x-scroll flex space-x-3">
+					{props.post.images.map((image: string, i: number) => {
+            if(i === 0){
+              return <img key={i} className="w-[70%] ml-20" src={image} alt="" />;
+            }
+						return <img key={i} className="w-[70%]" src={image} alt="" />;
+					})}
+				</div>
 
-  return (
-    <div className='w-full flex flex-col min-h-screen items-center mt-4'>
-      <div className='w-[90%] flex flex-col items-center'>
-        <span>{dummyProduct.name}</span>
-
-        <div className='w-full overflow-x-scroll flex space-x-3'>
-          {dummyProduct.images.map((image, i) => {
-            return (
-                <img key={i} className='w-[70%]' src={image} alt='' />
-            )
-          })}
-        </div>
-
-        {/* <img src={dummyProduct.defaultImage} alt='' /> */}
-        <div className='flex flex-col items-center space-y-4 mt-3'>
-          <span>${dummyProduct.price}</span>
-          <span>{dummyProduct.description}</span>
-          <div className='space-x-3'>
-            <button className='border border-black px-4 rounded-lg'>Go Back</button>
-            <button onClick={handleAddToCart} className='border border-black px-4 rounded-lg'>Add to cart</button>
-          </div>
-          {/* <button className='border border-black px-4 rounded-lg'>Add to cart</button> */}
-        </div>
-      </div>
-    </div>
-  )
-}
+				<div className="flex flex-col items-center space-y-4 mt-3">
+					<span>${props.post.price}</span>
+					<span>{props.post.description}</span>
+					<div className="space-x-3">
+						<button className="border border-black px-4 rounded-lg">
+							Go Back
+						</button>
+						<button
+							onClick={handleAddToCart}
+							className="border border-black px-4 rounded-lg"
+						>
+							Add to cart
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  // const connection = await connectToDatabase();
-  // const db = connection.db;
-
-  // const id = context.params.id;
-  // const res = await db.collection('products').findOne({ _id: ObjectId(id)});
-  const connection = await connectToDatabase();
+	const connection = await connectToDatabase();
 
 	const db = connection.db;
 
 	const allProducts = await db.collection("products").find({}).toArray();
 
-	const paths = allProducts.map((product: { _id: { toString: () => any; }; }) => {
+	const paths = allProducts.map((product: { _id: { toString: () => any } }) => {
 		return {
 			params: { id: product._id.toString() },
 		};
@@ -75,9 +66,9 @@ export const getStaticPaths: GetStaticPaths = async () => {
 		paths,
 		fallback: true,
 	};
-}
+};
 
-export const getStaticProps = async (context: { params: { id: string; }; }) => {
+export const getStaticProps = async (context: { params: { id: string } }) => {
 	const connection = await connectToDatabase();
 
 	const db = connection.db;
@@ -95,4 +86,4 @@ export const getStaticProps = async (context: { params: { id: string; }; }) => {
 	};
 };
 
-export default ProductDetails
+export default ProductDetails;
