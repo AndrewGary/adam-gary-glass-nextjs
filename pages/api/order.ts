ca548b33fbb7379d79bfb2a0c8826b5e582fb080
Brenda Gary <brendagary@Brendas-MacBook-Pro.local>
2022-11-21 23:29:13 -0600

    switching computers

--- pages/api/order.ts
+++ pages/api/order.ts
@@ -38,9 +38,23 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         case 'PUT': {
             console.log('req.body: ', req.body);
 
+            const filter = { _id: ObjectId(req.body._id) };
+                // this option instructs the method to create a document if no documents match the filter
+                const options = { upsert: true };
+                // create a document that sets the plot of the movie
+                const updateDoc = {
+                  $set: {
+                    paid: req.body.paid
+                  },
+                };
+                const result = await db.collection('orders').updateOne(filter, updateDoc, options);
+                console.log(
+                  `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`,
+                );
+
             // const yep = await db.collection('orders').findOneAndUpdate({_id: req.body._id}, {shipped: req.body.shipped, paid: req.body.paid});
 
-            const yep = await db.collection('orders').replaceOne({_id: req.body._id}, {...req.body})
+            // const yep = await db.collection('orders').replaceOne({_id: req.body._id}, {...req.body})
 
             // console.log('yep: ', yep);
             // const result = await db.collection('orders').findByIdAndUpdate(req.body._id, req.body)
@@ -48,7 +62,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             // console.log('result: ', result);
 
             // return res.status(200).json(result);
-            return res.status(200).json(yep);
+            return res.status(200).json({ message: 'hello'});
 
         }
     }
