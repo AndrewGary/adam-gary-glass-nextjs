b5e18a1e852d2672a0862a6deb2a43e0e0f7f5ef
Andrew Gary <Andrew.gary91@gmail.com>
2022-11-30 15:12:27 -0600

    switching to laptop because im headed to work

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,1 +1,19 @@
+import nodemailer from 'nodemailer';
+
 export const usStates = ['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY']
+
+const email = process.env.NEXT_PUBLIC_EMAIL;
+const pass = process.env.NEXT_PUBLIC_EMAIL_PASS;
+
+export const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: email,
+        password: pass
+    }
+})
+
+export const mailOptions = {
+    from:email,
+    to: email
+}
