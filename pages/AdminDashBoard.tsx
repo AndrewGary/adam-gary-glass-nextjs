1e730ff5bfc5524caf8c38a3ec9e89be26bb7def
Andrew <brendagary@Brendas-MacBook-Pro.local>
2022-12-31 05:56:11 -0600

    restyled the admin dashboard to have a simple look

--- pages/AdminDashBoard.tsx
+++ pages/AdminDashBoard.tsx
@@ -1,62 +1,161 @@
-import React from 'react'
-import Link from 'next/link';
-import { connectToDatabase } from '../mongoConnection';
+import React from "react";
+import Link from "next/link";
+import { connectToDatabase } from "../mongoConnection";
 
-type Props = { allOrders: any[]}
+type Props = { allOrders: any[] };
 
 export const getServerSideProps = async () => {
+  const connection = await connectToDatabase();
 
-    const connection = await connectToDatabase();
-  
-    const db = connection.db;
-  
-    const results = await db.collection('orders').find({}).toArray();
-  
-    const stringifiedResults = JSON.stringify(results);
-  
-    const allOrders = JSON.parse(stringifiedResults);
-  
-    return {
-      props: {
-        allOrders: allOrders
-      }
-    }
-  }
+  const db = connection.db;
+
+  const results = await db.collection("orders").find({}).toArray();
+
+  const stringifiedResults = JSON.stringify(results);
+
+  const allOrders = JSON.parse(stringifiedResults);
+
+  return {
+    props: {
+      allOrders: allOrders,
+    },
+  };
+};
 
 const AdminDashBoard = (props: Props) => {
+  return (
+    <div className="w-full flex flex-wrap justify-evenly">
+      <Link
+        href={"/"}
+        className=" w-44 h-44 button-styles flex justify-center items-center mt-1"
+      >
+        4 New Messages
+      </Link>
+
+      <Link
+        href={"/Orders"}
+        className=" w-44 h-44 button-styles flex justify-center items-center mt-1"
+      >
+        {props.allOrders.length} Open Orders
+      </Link>
+
+      <Link className="w-44 h-44 button-styles flex justify-center items-center mt-1" href={"/AddNewProduct"}>
+        Add New Product
+      </Link>
+      <Link className="w-44 h-44 button-styles flex justify-center items-center mt-1" href={"/EditProducts"}>
+        Edit Products
+      </Link>
+    </div>
+  );
 
   return (
-    <div className='w-full min-h-screen flex flex-col items-center space-y-4 pt-2'>
-        <Link href={'/'} className='w-[85%] border border-black rounded-md flex justify-center items-center h-16'>
-            4 New Messages
+    <div className="w-full min-h-screen flex flex-col items-center space-y-4 pt-2">
+      <Link
+        href={"/"}
+        className="w-[85%] button-styles flex justify-center items-center h-16"
+      >
+        4 New Messages
+      </Link>
+
+      <Link
+        href={"/Orders"}
+        className="w-[85%] button-styles flex justify-center items-center h-16"
+      >
+        {props.allOrders.length} Open Orders
+      </Link>
+
+      <span className="w-[85%] flex underline">Actions</span>
+      <div className="flex flex-col w-[85%] space-y-2">
+        <Link
+          className="pl-4 button-styles text-center"
+          href={"/AddNewProduct"}
+        >
+          Add New Product
         </Link>
-
-        <Link href={'/Orders'} className='w-[85%] border border-black rounded-md flex justify-center items-center h-16'>
-            {props.allOrders.length} Open Orders
+        <Link className="pl-4 button-styles text-center" href={"/EditProducts"}>
+          Edit Products
         </Link>
+      </div>
 
-        <span className='w-[85%] flex underline'>Actions</span>
-        <div className='flex flex-col w-[85%] space-y-2'>
-            <Link className='pl-4 border border-black rounded-md text-center' href={'/AddNewProduct'}>
-                Add New Product
-            </Link>
-            <Link className='pl-4 border border-black rounded-md text-center' href={'/EditProducts'}>
-                Edit Products
-            </Link>
-        </div>
+      <button
+        className="button-styles"
+        onClick={() => {
+          const reqOpts = {
+            method: "DELETE",
+          };
 
-        <button className='button-styles' onClick={() => {
-            const reqOpts = {
-                method: 'DELETE'
-            }
-
-            fetch('/api/products', reqOpts)
-            .then(resp => {
-                console.log(resp);
-            })
-        }}>Delete All Products</button>
+          fetch("/api/products", reqOpts).then((resp) => {
+            console.log(resp);
+          });
+        }}
+      >
+        Delete All Products
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default AdminDashBoard
+export default AdminDashBoard;
+
+// import React from 'react'
+// import Link from 'next/link';
+// import { connectToDatabase } from '../mongoConnection';
+
+// type Props = { allOrders: any[]}
+
+// export const getServerSideProps = async () => {
+
+//     const connection = await connectToDatabase();
+
+//     const db = connection.db;
+
+//     const results = await db.collection('orders').find({}).toArray();
+
+//     const stringifiedResults = JSON.stringify(results);
+
+//     const allOrders = JSON.parse(stringifiedResults);
+
+//     return {
+//       props: {
+//         allOrders: allOrders
+//       }
+//     }
+//   }
+
+// const AdminDashBoard = (props: Props) => {
+
+//   return (
+//     <div className='w-full min-h-screen flex flex-col items-center space-y-4 pt-2'>
+//         <Link href={'/'} className='w-[85%] button-styles flex justify-center items-center h-16'>
+//             4 New Messages
+//         </Link>
+
+//         <Link href={'/Orders'} className='w-[85%] button-styles flex justify-center items-center h-16'>
+//             {props.allOrders.length} Open Orders
+//         </Link>
+
+//         <span className='w-[85%] flex underline'>Actions</span>
+//         <div className='flex flex-col w-[85%] space-y-2'>
+//             <Link className='pl-4 button-styles text-center' href={'/AddNewProduct'}>
+//                 Add New Product
+//             </Link>
+//             <Link className='pl-4 button-styles text-center' href={'/EditProducts'}>
+//                 Edit Products
+//             </Link>
+//         </div>
+
+//         <button className='button-styles' onClick={() => {
+//             const reqOpts = {
+//                 method: 'DELETE'
+//             }
+
+//             fetch('/api/products', reqOpts)
+//             .then(resp => {
+//                 console.log(resp);
+//             })
+//         }}>Delete All Products</button>
+//     </div>
+//   )
+// }
+
+// export default AdminDashBoard
