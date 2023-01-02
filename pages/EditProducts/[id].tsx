5a56d6b92bc0c77a99cb22d9cbcfb204ad15213c
Andrew Gary <Andrew.gary91@gmail.com>
2023-01-02 16:28:23 -0600

    can now add new images to the product

--- pages/EditProducts/[id].tsx
+++ pages/EditProducts/[id].tsx
@@ -1,25 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useRouter } from "next/router";
 import { CldUploadButton } from "next-cloudinary";
-
 
 type Props = {};
 
 interface Product {
-  _id: string;
-  name: string;
-  description: string;
-  price: number | undefined;
-  quantity: number | undefined;
-  images: string[]
-  defaultImage: string;
+	_id: string;
+	name: string;
+	description: string;
+	price: number | undefined;
+	quantity: number | undefined;
+	images: string[];
+	defaultImage: string;
 }
 
 const EditSpecificItem = (props: Props) => {
 	const router = useRouter();
 
 	const [productBeingEdited, setProductBeingEdited] = useState<any>({
-        _id: '',
+		_id: "",
 		name: "",
 		description: "",
 		price: undefined,
@@ -28,7 +27,7 @@ const EditSpecificItem = (props: Props) => {
 		defaultImage: "",
 	});
 	const [formValues, setFormValues] = useState<any>({
-        _id: '',
+		_id: "",
 		name: "",
 		description: "",
 		price: undefined,
@@ -37,19 +36,25 @@ const EditSpecificItem = (props: Props) => {
 		defaultImage: "",
 	});
 
-    const [addedImages, setAddedImages] = useState([]);
+	let count = 0;
+	const uploadedImages = useRef<string[]>([]);
+	const setUploadedImages = (newImage: string) => {
+
+		uploadedImages.current.push(newImage);
+		setFormValues({
+			...formValues,
+			images: [...formValues.images, ...uploadedImages.current],
+		});
+	};
+	
 
 	useEffect(() => {
 		const asyncUseEffect = async () => {
 			const resp = await fetch(`/api/products/${router.query.id}`);
 
 			const parsedResp = await resp.json();
+			console.log('parsedResp: ', parsedResp)
 
-			console.log("parsedResp: ", parsedResp);
-
-      console.log('typeof parsedResp._id: ', typeof parsedResp._id);
-
-			console.log("PARSEDRESP: ", parsedResp);
 			const myFormValues: any = {
 				_id: parsedResp._id,
 				name: parsedResp.name,
@@ -60,7 +65,7 @@ const EditSpecificItem = (props: Props) => {
 				defaultImage: parsedResp.defaultImage,
 			};
 
-			console.log("fetched formValues: ", formValues);
+			// console.log("fetched formValues: ", formValues);
 			setFormValues(myFormValues);
 
 			setProductBeingEdited(myFormValues);
@@ -79,79 +84,102 @@ const EditSpecificItem = (props: Props) => {
 	const handleSubmit = async (e: React.FormEvent) => {
 		e.preventDefault();
 
-    let newValues: any = {};
-
-    
+		let newValues: any = {};
 
 		for (const key in formValues) {
-				if (formValues[key] !== productBeingEdited[key]) {
-console.log('formValues[key]: ', formValues[key]);
-console.log('productBeingEdited[key]: ', productBeingEdited[key])
-          if(key === 'price' || key === 'quantity'){
-            newValues[key] = parseInt(formValues[key]);
-          }else{
-            newValues[key] = formValues[key];
-          }
+			if (formValues[key] !== productBeingEdited[key]) {
+				console.log("formValues[key]: ", formValues[key]);
+				console.log("productBeingEdited[key]: ", productBeingEdited[key]);
+				if (key === "price" || key === "quantity") {
+					newValues[key] = parseInt(formValues[key]);
+				}else if(key === 'images'){
+					newValues[key] = [...formValues[key]]
+				} else {
+					newValues[key] = formValues[key];
+				}
 			}
 		}
 
-    const reqOptions = {
-      method: 'PUT',
+		const reqOptions = {
+			method: "PUT",
 			headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(newValues),
-    }
+			body: JSON.stringify(newValues),
+		};
 
-    const yeahhh = await fetch(`/api/products/${formValues._id}`, reqOptions);
+		const yeahhh = await fetch(`/api/products/${formValues._id}`, reqOptions);
 
-    const jeah = await yeahhh.json();
+		const jeah = await yeahhh.json();
 
-    console.log('JEAH: ', jeah);
+		console.log("JEAH: ", jeah);
 
-    //COME BACK HERE AND HANDLE IF IT WAS SUCCESSFULL OR NOT
+		//COME BACK HERE AND HANDLE IF IT WAS SUCCESSFULL OR NOT
 	};
 
 	return (
 		<div className="w-full min-h-screen flex flex-col items-center">
-            <h2 className="w-[70%] text-center mb-2">Change Default Image by selecting the image you want
-            </h2>
-            <div className={`flex flex-wrap justify-evenly w-full`}>
-                {formValues.images.map((image: any, i: number) => {
-                    return <img onClick={() => {
-                        setFormValues({
-                            ...formValues,
-                            defaultImage: image
-                        })
-                    }} className={`w-20 ${image === formValues.defaultImage ? 'border-2 border-red-500' : ''}`} src={image} alt='' />
-                })}
-            </div>
+			<h2 className="w-[70%] text-center mb-2">
+				Change Default Image by selecting the image you want
+			</h2>
+			<div className={`flex flex-wrap justify-evenly w-full`}>
+				{formValues.images.map((image: any, i: number) => {
+					return (
+						<img
+							onClick={() => {
+								setFormValues({
+									...formValues,
+									defaultImage: image,
+								});
+							}}
+							className={`w-20 ${
+								image === formValues.defaultImage
+									? "border-2 border-red-500"
+									: ""
+							}`}
+							src={image}
+							alt=""
+						/>
+					);
+				})}
+			</div>
 
-            <div className={`flex flex-wrap justify-evenly w-full`}>
-                {addedImages.map((image: any, i: number) => {
-                    return <img onClick={() => {
-                        setFormValues({
-                            ...formValues,
-                            defaultImage: image
-                        })
-                    }} className={`w-20 ${image === formValues.defaultImage ? 'border-2 border-red-500' : ''}`} src={image} alt='' />
-                })}
-            </div>
+			<div className={`flex flex-wrap justify-evenly w-full`}>
+				{uploadedImages.current.map((image: any, i: number) => {
+					return (
+						<img
+							onClick={() => {
+								setFormValues({
+									...formValues,
+									defaultImage: image,
+								});
+							}}
+							className={`w-20 ${
+								image === formValues.defaultImage
+									? "border-2 border-red-500"
+									: ""
+							}`}
+							src={image}
+							alt=""
+						/>
+					);
+				})}
+			</div>
 
-            <CldUploadButton
-					className="border border-black px-3 rounded-lg"
-					onUpload={(error: any, result: any, widget: any) => {
-                        // console.log('hello');
-						// setUploadedImages(result.info.secure_url);
-                        // console.log(result.info.secure_url)
-                        // setFormValues({
-                        //     ...formValues,
-                        //     images: [...formValues.images, result.info.secure_url]
-                        // })
-                        setAddedImages([...addedImages, result.info.secure_url])
-					}}
-					uploadPreset="product upload"
-				>
-					Upload Images Here
-				</CldUploadButton>
+			<CldUploadButton
+				className="border border-black px-3 rounded-lg"
+				onUpload={(error: any, result: any, widget: any) => {
+					// console.log('hello');
+					// setUploadedImages(result.info.secure_url);
+					// console.log(result.info.secure_url)
+					// setFormValues({
+					//     ...formValues,
+					//     images: [...formValues.images, result.info.secure_url]
+					// })
+					setUploadedImages(result.info.secure_url);
+				}}
+				uploadPreset="product upload"
+			>
+				Upload Images Here
+			</CldUploadButton>
 			<form className="w-[90%] flex flex-col">
 				<label htmlFor="name">Name</label>
 				<input
@@ -204,7 +232,7 @@ console.log('productBeingEdited[key]: ', productBeingEdited[key])
 				</div>
 				<button
 					onClick={() => {
-						console.log(formValues);
+						console.log()
 					}}
 					className="button-styles"
 				>
