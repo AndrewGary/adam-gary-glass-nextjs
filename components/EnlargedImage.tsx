501734e6ca7e33f0d15d02b826045ccffacd9ff8
Andrew Gary <Andrew.gary91@gmail.com>
2023-01-03 06:33:21 -0600

    working on the focused image component

---
+++ components/EnlargedImage.tsx
@@ -0,0 +1,37 @@
+// import React from 'react'
+
+// type Props = {image: string}
+
+// const EnlargedImage: React.FC<Props> = ({image}) => {
+//   return (
+//     <div className='absolute flex w-full min-h-screen justify-center items-centers z-10'>
+//       <div className='bg-black'>
+//         <img className=' h-96' src={image} alt={image} />
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default EnlargedImage
+
+import React, { useState } from 'react'
+
+type Props = {image: string}
+
+const EnlargedImage: React.FC<Props> = ({image}) => {
+
+  const [darkMode, setDarkMode] = useState(true);
+  
+  return (
+    
+      <div className={` relative ${darkMode ? 'bg-black' : 'bg-white'} rounded-md bg-opacity-90`}>
+        <img className=' h-96 m-5' src={image} alt={image} />
+        <img src={darkMode ? '/NightIcon.png' : '/DayIcon.png'} alt=''  className='absolute left-2 top-2 hover:opacity-50 transition-all' onClick={() => {
+          setDarkMode(!darkMode)
+        }}/>
+        <img src={darkMode ? '/NightExit.png' : '/DayExit.png'} alt=''  className='absolute z-10' onClick={() => {setDarkMode(false)}}/>
+      </div>
+  )
+}
+
+export default EnlargedImage
